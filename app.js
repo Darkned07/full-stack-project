e4c7e5d7e28e9a53c45b107103ee9ca829fc5b24
Darkned07 <madaminovmuhammadyusuf806@gmail.com>
2024-12-12 22:33:28 +0500

    u

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Listening on - http://localhost:${PORT}`));
+app.listen(PORT);
 
 const bootstrap = async () => {
   try {
